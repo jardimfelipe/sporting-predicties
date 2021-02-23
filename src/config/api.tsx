8ad2abfa35d5c7973f891d3919856b8acfed2bfd
Felipe Jardim Evangelista <felipe.evangelista@b2wdigital.com>
2021-02-23 16:48:52 -0300

    feature: axios header

--- src/config/api.tsx
+++ src/config/api.tsx
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_URL_API,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
 });
 
 export default api;
